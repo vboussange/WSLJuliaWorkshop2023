0a529c47491f5b9b6f94f4abea2fe83c24b6c828
Mauro Werder <mauro3@runbox.com>
2023-03-27 11:20:04 +0200

    Ice thickness exercise

--- Day2/11_geodata/deploy.jl
+++ Day2/11_geodata/deploy.jl
@@ -7,7 +7,7 @@ Literate.markdown("11_geodata.jl",
                   credit=false, execute=false, mdstrings=true, flavor=Literate.CommonMarkFlavor())
 
 Literate.notebook("11_geodata-ex.jl",
-                  credit=false, execute=true, mdstrings=true)
+                  credit=false, execute=false, mdstrings=true)
 
 Literate.markdown("11_geodata-ex.jl",
                   credit=false, execute=true, mdstrings=true, flavor=Literate.CommonMarkFlavor())
